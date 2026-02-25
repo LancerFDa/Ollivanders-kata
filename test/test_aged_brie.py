89f4d2d79ef502aeb0b9244dfe2132c86c1768a5
LancerFDa <rodrgolorenzo@gmail.com>
2026-02-25 13:54:39 +0100

    refactor(Refactorizar clase NormalItem y Updateable):

--- test/test_aged_brie.py
+++ test/test_aged_brie.py
@@ -1,7 +1,4 @@
-import sys, os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
-
-from gilded_rose import Item, GildedRose
+from src.gilded_rose import Item, GildedRose
 
 
 def test_crear_aged_brie():
